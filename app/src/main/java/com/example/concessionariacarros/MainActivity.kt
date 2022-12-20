c51f9eac8882339e89714ebdc78590274993c6e7
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-20 00:41:48 -0300

    feat: create carroCard

--- app/src/main/java/com/example/concessionariacarros/MainActivity.kt
+++ app/src/main/java/com/example/concessionariacarros/MainActivity.kt
@@ -4,46 +4,51 @@ package com.example.concessionariacarros
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.concessionariacarros.model.Carro
+import com.example.concessionariacarros.model.enum.TipoVeiculo
+
+import android.content.Context
+import android.widget.Toast
+import androidx.activity.compose.setContent
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.concessionariacarros.model.Carro
-import com.example.concessionariacarros.model.enum.TipoVeiculo
-import com.example.concessionariacarros.ui.theme.InserirCarrosTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,6 +58,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Composable
 fun BuildLayout() {
@@ -80,16 +86,19 @@ fun CarrosCard(carro: Carro){
                 expandDetails = !expandDetails
             },
         elevation = 4.dp
-    ) {
+    ){
+        Row{
+            Column(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .height(60.dp)
+                    .weight(0.5f)
 
-        Column(
-            modifier = Modifier
-                .background(Color.Green)
-                .fillMaxWidth()
-                .padding(16.dp))
-        {
-
-            Row{
+            ){
+                Text(text = "")
+            }
+            Column(Modifier.weight(15f)
+            ){
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
@@ -99,7 +108,7 @@ fun CarrosCard(carro: Carro){
                         }
                 ) {
                     Text(
-                        text = "Model: " + carro.modelo,
+                        text = "Modelo: " + carro.modelo,
                         textAlign = TextAlign.Center,
                         fontSize = 24.sp,
                         overflow = TextOverflow.Ellipsis,
@@ -123,59 +132,16 @@ fun CarrosCard(carro: Carro){
                         Text(
                             text = stringResource(
                                 id = R.string.description_text,
+                                carro.placa,
                                 carro.tipo.descricao,
-                                carro.placa, carro.status
+                                carro.status
                             ),
                             modifier = Modifier.padding(8.dp)
                         )
                     }
                 }
             }
-
         }
-
-//        Row{
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(16.dp)
-//                    .clickable {
-//                        expandDetails = !expandDetails
-//                    }
-//            ) {
-//                Text(
-//                    text = "Model: " + carro.modelo,
-//                    textAlign = TextAlign.Center,
-//                    fontSize = 24.sp,
-//                    overflow = TextOverflow.Ellipsis,
-//                    maxLines = 1,
-//                    modifier = Modifier
-//                        .widthIn(0.dp, 250.dp)
-//                        .clickable {
-//                            expandDetails = !expandDetails
-//                        }
-//                )
-//                Row(
-//                    modifier =
-//                    Modifier.align(Alignment.CenterHorizontally)
-//                ) {
-//                }
-//                AnimatedVisibility(
-//                    visible = expandDetails,
-//                    enter = fadeIn(initialAlpha = 0f) + expandVertically(),
-//                    exit = fadeOut(animationSpec = tween(durationMillis = 250)) + shrinkVertically()
-//                ) {
-//                    Text(
-//                        text = stringResource(
-//                            id = R.string.description_text,
-//                            carro.tipo.descricao,
-//                            carro.placa, carro.status
-//                        ),
-//                        modifier = Modifier.padding(8.dp)
-//                    )
-//                }
-//            }
-//        }
 
     }
 }
